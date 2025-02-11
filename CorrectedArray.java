2451f8f3b3854a853fd2234b810ecafe55402c5d
BugHunter-X <askzenai@gmail.com>
2025-02-11 10:46:05 +0100

    Add CorrectedArray.java file

---
+++ CorrectedArray.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) { // Corrected loop condition
+            arr[i] = i * 2;
+        }
+        //To prevent ArrayIndexOutOfBoundsException 
+        //System.out.println(arr[5]);
+        for(int i = 0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
